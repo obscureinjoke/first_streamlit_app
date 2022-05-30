8da1856ace9bc523d99e3f667d96c2e17401e00a
obscureinjoke <firedrake@gmail.com>
2022-05-30 09:54:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ st.title('New Diner!')
 st.header('Menu')
 
 st.text('Food items!')
-fruits_selected=st.multiselect("Pick some fruits!",list(my_fruit_list.index), ['Strawberries'])
+fruits_selected=st.multiselect("Pick some fruits!",list(my_fruit_list.index), ['Avocado'],['Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]
-st.dataframe(my_fruit_list)
+st.dataframe(fruits_to_show)
