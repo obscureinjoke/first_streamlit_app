0016e135b2fe153c575cc5643e9e657ad3fb6282
obscureinjoke <firedrake@gmail.com>
2022-05-30 09:32:54 +0100

    Update streamlit_app.py
    Initial pandas integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pa
+
+my_fruit_list = pa.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 st.title('New Diner!')
 
 st.header('Menu')
 
 st.text('Food items!')
+streamlit.dataframe(my_fruit_list)
