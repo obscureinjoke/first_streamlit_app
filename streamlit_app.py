1f4f8668fb03168225cb357f03b7e7cee2f8144d
obscureinjoke <firedrake@gmail.com>
2022-06-07 17:03:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import snowflake.connector
 
 my_cnx=snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("SELECT * from fruit_load_list")
 #got_row = my_cur.fetchone()
 st.header('List contains')
 #st.write('Hello')
