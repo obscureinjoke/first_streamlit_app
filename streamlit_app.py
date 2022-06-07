f3543444dde7fa17cb8806eaba65865d1ec32b52
obscureinjoke <firedrake@gmail.com>
2022-06-07 16:54:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import requests
 import snowflake.connector
 
 my_cnx=snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur=my_cnx.cursor()
-my_cur.execute("SELECT_CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
-my_data_row=my_cur.fetch_one()
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
 st.text=('Connection test')
 st.text(my_data_row)
 
