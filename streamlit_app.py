c1df8b8a59414718516350f376b3cf73376443c0
obscureinjoke <firedrake@gmail.com>
2022-06-07 16:15:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import requests
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-get_fruit=st.input('What fruit would you like to know about?', 'Kiwi')
+get_fruit=st.text_input('What fruit would you like to know about?', 'Kiwi')
 st.write('User entered ',get_fruit)
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+get_fruit)
 fruityvice_normalised=pandas.json_normalize(fruityvice_response.json())
